cf5cd5e70a390ecc7e3a3329925408e693e14e29
Yakov Rogozhin <yorikup198@gmail.com>
2018-02-17 13:25:01 +0300

    Done!

--- renamer.js
+++ renamer.js
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+const fs = require('fs');
+const path = require('path');
+
+const newLineChar = require('os').EOL;
+var filesCounter = 1;
+
+fs.writeFile('renamed.txt', '', (err) => {
+  if (err) throw err;
+});
+
+fs.readdir('./', (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    if (path.extname(file) === '.mp4') {
+      let fileName = filesCounter + '. ' + file;
+
+      fs.appendFileSync('renamed.txt', `${newLineChar}${fileName}`);
+      fs.renameSync('./' + file, './' + filesCounter + '.mp4');
+
+      filesCounter++;
+      console.log(fileName + ' processed!');
+    };
+  });
+});
